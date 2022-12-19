9c829baf4c66206027c3b0dd5b18479be39b4a4a
silaozinan <98406763+silaozinan@users.noreply.github.com>
2022-12-19 20:41:14 +0300

    patients modified, minor mistakes fixed in others

--- patients/insert.php
+++ patients/insert.php
@@ -1,6 +1,3 @@
-
-
-
 <body style = 'background-color:  #DBF9FC;color: black;font-weight: 2000;font-style: italic;font-size: 20;text-align:left;'>
 
 
@@ -22,28 +19,32 @@ if (!empty($_POST['p_name']) and  !empty($_POST['age']) and !empty($_POST['P_all
     $sql_statement = "INSERT into patients(p_name,age,P_allergicreaction,p_diagnosis,p_address,gender)
     VALUES  ('$sname',$age,'$P_allergicreaction','$p_diagnosis','$p_address','$gender')"; 
 
+
     $result = mysqli_query($hospital_db, $sql_statement);
     if($result == 1)
     {
         echo "Succesfully inserted the patient: ". $sname. "<br>";
+        $newURL = "index.php";
+        header( "refresh:2;url=".$newURL );
     }
     else
     {
         //echo "<h3 style='color: rgb(105, 0, 166);font-weight: 500;font-size: 20;padding-bottom:0px;'>". "Filtered Data:."."</h3>";
         echo "Problem occurred while inserting." ;
+        $newURL = "insert_patient.html";
+        echo "<Br>You are redirecting to the insertion page..." ;
+        header( "refresh:5;url=".$newURL );
     }
+
+    } 
+    else 
+    {
     $newURL = "insert_patient.html";
-    echo "<Br>You are redirecting to the insertion page..." ;
-    header( "refresh:5;url=".$newURL );
-} 
-else 
-{
-    $newURL = "insert_patient.html";
-    
+
     echo "Missing information. Please fill in all of the blanks." ."<Br><Br><Br><Br>".  "You are redirecting to the insertion page..." ;
     header( "refresh:5;url=".$newURL );
 
-}
+    }
 
 ?>
 </body>
--- patients/selection.php
+++ patients/selection.php
@@ -1,12 +1,154 @@
-<form action="selection.php" method="POST" accept-charset="utf-8" target = "_self" > 
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Patient</title>
+
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 50%;
+  
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+  
+  
+}
+p{
+   text-align:center;
+   padding: 8px;
+   border-radius: 5px;
+  
+    
+}
+tr:nth-child(even) {
+  background-color: #dddddd;
+  background-color: #e4e4e4;
+  background-color: #efecec;
+  background-color:#82C3EC;
+  background-color: #e4e4e4;
+  background-color: #efecec;
+}
+tr:nth-child(odd) {
+  background-color: white;
+  
+}
+tr:nth-child(1) {
+  background-color: #76a5af;
+  background-color: rgb(105, 0, 166);
+  color: #82C3EC;
+  font: weight 500px;
+  font-style: italic;
+}
+tr.a:hover{
+  background-color: coral;
+}
+body{
+    text-align:center;
+    background-image:url(pht.jpg);background-size: 100% 100%;background-attachment: fixed;
+}
+h5{
+    padding:30px;
+    color: rgb(6, 123, 248);
+    text-shadow:3px 3px rgb(96, 187, 187);
+    font-weight: 1000;
+    font-size: 50px;
+    padding-bottom:0px;
+}
+h4{
+    color: rgb(6, 123, 248);
+    font-weight:2000;
+    padding:10px;
+}
+input[type=submit] {
+  width: 50%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50vh;
+}
+
+.container a {
+  font-size: 1.5rem;
+  padding: 1rem 3rem;
+  color: #f4f4f4;
+  text-transform: uppercase;
+}
+
+.btn {
+  text-decoration: none;
+  border: 1px solid rgb(146, 148, 248);
+  position: relative;
+  overflow: hidden;
+}
+
+.btn:hover {
+  box-shadow: 1px 1px 25px 10px rgba(146, 148, 248, 0.4);
+}
+
+.btn:before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: -100%;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    120deg,
+    transparent,
+    rgba(146, 148, 248, 0.4),
+    transparent
+  );
+  transition: all 650ms;
+}
+
+.btn:hover:before {
+  left: 100%;
+}
+h6{
+    padding: 0px;
+    margin: 0px;
+    background-color: white;
+    color: red;
+    font-size:19px;
+}
+</style>
+
+</head>
+<body>
+<form action="selection.php" method="POST" accept-charset="utf-8" target = "_self" style="align=center" > 
  
     
-    <body style = ";font-weight:1000;text-align: center;font-style: italic;background-color: rgb(96, 187, 187);margin-top:100px;background-image:url(pht.jpg);background-size: 100% 100%;background-attachment: fixed">
-
-    <h3 style="color: rgb(6, 123, 248);text-shadow:3px 3px rgb(96, 187, 187);font-weight: 1000;font-size: 40;padding-bottom:0px;"> Filter The Patients</h3>
+    
+    <h5> Filter The Patients</h5>
         
          
-        Gender:
+        
+
+    
+    
+    <h4>
+
+    GENDER:
         <select name="gender" style = "width: 150px;">
         <option value="3">All</option>
         <option value="1">Male</option>
@@ -15,36 +157,46 @@
         </select>
 
     
-    Allergic Reaction: <select name="allergicreaction" style = "width: 150px;">
+    ALLERGIC REACTION: <select name="allergicreaction" style = "width: 150px;">
             <option value="3">All</option>
             <option value="1">Yes</option>
             <option value="2">No</option>
            
             </select>
-    <br><br>Address:
+    <br><br>ADDRESS:
         <input type="text" id="address" name="address" style = "width: 150px;"> 
-   Diagnosis:
+   DIAGNOSIS:
         <input type="text" id="diagnosis" name="diagnosis" style = "width: 150px;"> 
 
-    Min Age : 
+    MIN AGE : 
         <input type ="text" id = "age_min" name = "age_min" style = "width: 150px;" >    
-    Max Age :
+    MAX AGE :
         <input type ="text" id = "age_max" name = "age_max"  style = "width: 150px;">    
 
     
+    
+      </h4>
+ 
     <br><br> <input type="submit" value="Filter"
                 style="background-color:rgb(242, 142, 142);
                 border: 2px solid #0a0909;" >
-                
-
-    </body>
+    
 
 
-<?php 
+</form>
+<div align="center">
 
-include "config.php"; // Makes mysql connection
+	<table>
+
+
+<tr> <th> PATIENT NAME </th><th> PATIENT ID </th> <th> AGE</th><th> ALLERGIC REACTION </th><th> DIAGNOSIS </th><th> ADDRESS </th><th> GENDER </th></tr> 
+<?php
+
+include "config.php";
 if(!empty($_POST))
 {
+
+    
     if (!empty($_POST['address']) )
     {  $address = $_POST['address'];}
     if (!empty($_POST['diagnosis']) )
@@ -101,17 +253,12 @@ if(!empty($_POST))
             $sql_statement = "SELECT * FROM patients WHERE P_allergicreaction = '$allergicreaction' AND age <= $age_max AND age >= $age_min" ;
         else
             $sql_statement = "SELECT * FROM patients WHERE age <= $age_max AND age >= $age_min ";
-    
     }
-    #$sql_statement = "SELECT * FROM patients WHERE gender = '$gender' AND P_allergicreaction = '$allergicreaction' AND p_diagnosis = '$diagnosis'AND age < $age_max AND age > $age_min AND p_address = '$address'";
-    #echo  $address;
-    #     "; 
-    #$sql_statement = "SELECT * FROM patients WHERE p_address = '$address'";
-    
-    $result = mysqli_query($hospital_db, $sql_statement); // Executing query
+        
     
     
-    
+ 
+    $result = mysqli_query($hospital_db, $sql_statement);
     if (mysqli_num_rows($result) == 0)
     {
         echo "<h3 style='color: rgb(105, 0, 166);font-weight: 500;font-size: 20;padding-bottom:0px;'>". "Filtered Data:."."</h3>";
@@ -119,9 +266,8 @@ if(!empty($_POST))
     }
     else  
     {
-        echo "<h3 style='color: rgb(105, 0, 166);font-weight: 500;font-size: 20;padding-bottom:0px;'>". "Filtered Data:."."</h3>";
+        echo "<h3 style='color: rgb(105, 0, 166);font-weight: 500;font-size: 20;padding-bottom:0px;'>". "Filtered Data:"."</h3>";
     }    
-
     while($row = mysqli_fetch_assoc($result)) { // Iterating the result
         $patient_id = $row['p_id']; 
         #$patient_name  = iconv('UTF-8', 'ASCII//TRANSLIT', $row['p_name']);
@@ -131,14 +277,20 @@ if(!empty($_POST))
         $diagnosis = $row['p_diagnosis']; 
         $address = $row['p_address']; 
         $gender = $row['gender']; 
-        echo $patient_id . " " . $patient_name . " " . $age . " ".$allergy . " " .$diagnosis . " " .$address . " " .$gender . "<br>"; 
+        echo "<tr class='a'>" . "<th>" . $patient_name . "</th>"  . "<th>" . $patient_id ."</th>" . "<th>" . $age . "</th>". "<th>" . $allergy . "</th>". "<th>" . $diagnosis . "</th>" . "<th>" . $address . "</th>"."<th>" . $gender . "</th>". "</tr>" ;          
     }
-
-}
-
-
+}       
 
 
 ?>
-</form>
 
+</table>
+
+<div class="container">
+      <a href="index.php" class="btn">Go back to main page</a>
+    </div>
+
+</div>
+
+</body>
+</html>
